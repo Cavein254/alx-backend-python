e74ea1500aa3b4a72c771ab770f5ac8257f0a03a
johnmuthua <caveomuthama@gmail.com>
2025-07-12 09:02:09 +0300

    fix: add datetime

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -1,5 +1,6 @@
 import sqlite3
 import functools
+from datetime import datetime
 
 # Decorator to log SQL queries
 
@@ -7,7 +8,8 @@ def log_queries(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         query = args[0] if args else kwargs.get('query', '')
-        print(f"Executing query: {query}")
+        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        print(f"[{timestamp}] [SQL LOG] Executing query: {query}")
         return func(*args, **kwargs)
     return wrapper
 
