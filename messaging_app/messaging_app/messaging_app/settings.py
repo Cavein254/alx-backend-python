38ac297219cfa42c03f7fd599905343a16c9f340
johnmuthua <caveomuthama@gmail.com>
2025-07-17 13:22:32 +0300

    fix: remove unused authentication class

--- messaging_app/messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/messaging_app/settings.py
@@ -130,6 +130,5 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.BasicAuthentication',
     ],
 }
