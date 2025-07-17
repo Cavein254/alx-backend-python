5a1b26ee8fb5fe3f03da4c39a3a4676afd2a835e
johnmuthua <caveomuthama@gmail.com>
2025-07-17 14:07:36 +0300

    feat: add filters to views

---
+++ messaging_app/chats/urls.py
@@ -0,0 +1,10 @@
+# chats/urls.py
+
+from rest_framework.routers import DefaultRouter
+from .views import ConversationViewSet, MessageViewSet
+
+router = DefaultRouter()
+router.register(r'conversations', ConversationViewSet, basename='conversations')
+router.register(r'messages', MessageViewSet, basename='messages')
+
+urlpatterns = router.urls
--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, permissions, status
+from rest_framework import viewsets, permissions, filters
 from rest_framework.response import Response
 from .models import Conversation, Message, User
 from .serializers import ConversationSerializer, MessageSerializer
@@ -7,27 +7,23 @@ class ConversationViewSet(viewsets.ModelViewSet):
     queryset = Conversation.objects.all()
     serializer_class = ConversationSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['created_at']
 
     def get_queryset(self):
-        # Show only conversations the user is part of
         return self.queryset.filter(participants=self.request.user)
 
-    def perform_create(self, serializer):
-        # Automatically include the current user in participants
-        conversation = serializer.save()
-        conversation.participants.add(self.request.user)
-
 class MessageViewSet(viewsets.ModelViewSet):
+    queryset = Message.objects.all()
     serializer_class = MessageSerializer
     permission_classes = [permissions.IsAuthenticated]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['text']  # allow ?search=hello
+    ordering_fields = ['timestamp']  # allow ?ordering=-timestamp
 
     def get_queryset(self):
-        conversation_id = self.kwargs.get('conversation_pk')
-        return Message.objects.filter(conversation_id=conversation_id)
-
-    def perform_create(self, serializer):
-        conversation_id = self.kwargs.get('conversation_pk')
-        serializer.save(
-            sender=self.request.user,
-            conversation_id=conversation_id
-        )
+        queryset = super().get_queryset()
+        conversation_id = self.request.query_params.get('conversation')
+        if conversation_id:
+            queryset = queryset.filter(conversation_id=conversation_id)
+        return queryset
