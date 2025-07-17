b8f7dd8df208ff89b34815a41328b6bd40d8e6d9
johnmuthua <caveomuthama@gmail.com>
2025-07-17 14:03:21 +0300

    feat: add views

--- messaging_app/chats/views.py
+++ messaging_app/chats/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
+from .models import Conversation, Message, User
+from .serializers import ConversationSerializer, MessageSerializer
 
-# Create your views here.
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        # Show only conversations the user is part of
+        return self.queryset.filter(participants=self.request.user)
+
+    def perform_create(self, serializer):
+        # Automatically include the current user in participants
+        conversation = serializer.save()
+        conversation.participants.add(self.request.user)
+
+class MessageViewSet(viewsets.ModelViewSet):
+    serializer_class = MessageSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        conversation_id = self.kwargs.get('conversation_pk')
+        return Message.objects.filter(conversation_id=conversation_id)
+
+    def perform_create(self, serializer):
+        conversation_id = self.kwargs.get('conversation_pk')
+        serializer.save(
+            sender=self.request.user,
+            conversation_id=conversation_id
+        )
