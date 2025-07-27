f8a64bb895702488a814a4cd55e55c05ef30528a
johnmuthua <caveomuthama@gmail.com>
2025-07-27 17:19:37 +0300

    feat: add jwt

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'chats',  
 ]
 
@@ -133,5 +134,6 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
 }
