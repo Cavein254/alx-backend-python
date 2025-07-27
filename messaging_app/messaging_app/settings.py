05f7c0b55c455351a9701966b6853f0193a67e5c
johnmuthua <caveomuthama@gmail.com>
2025-07-27 18:19:22 +0300

    chore: add pagination

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -138,6 +138,9 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'DEFAULT_PAGINATION_CLASS': 'chats.pagination.MessagePagination',
     'PAGE_SIZE': 20,
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ],
 }
