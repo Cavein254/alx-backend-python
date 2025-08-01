e933855f9911cc01112eb6ea3d783390cb0f655d
johnmuthua <caveomuthama@gmail.com>
2025-08-01 10:50:30 +0300

    feat: add admin model

--- Django-signals_orm-0x04/messaging/admin.py
+++ Django-signals_orm-0x04/messaging/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from messaging.models import Message, Notification
+
+@admin.register(Message)
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('sender', 'receiver', 'timestamp')
+    search_fields = ('sender__username', 'receiver__username', 'content')
+    list_filter = ('timestamp',)
+
+@admin.register(Notification)
+class NotificationAdmin(admin.ModelAdmin):
+    list_display = ('user', 'message', 'is_read', 'created_at')
+    search_fields = ('user__username', 'message__content')
+    list_filter = ('is_read', 'created_at')
